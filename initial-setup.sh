d2b0b9ca334664d9de7643b351d6db8698914bc1
Kenaniah Cerny <kenaniah@gmail.com>
2014-04-25 18:10:47 -0700

    Added initial setup script
    Also testing line ending conversion from windows

---
+++ initial-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is the initial setup script that should be executed when the server
+# is first provisioned. It will proceed to update pacakges and install the 
+# software config stack.
+
+yum update -y
