e954e7879ad3acfe67fc7031e3346ebcc7e29dbc
Kenaniah Cerny <kcerny@wirelesscapital.com>
2014-04-26 13:09:36 -0700

    Updated directory names

--- perform-setup.sh
+++ perform-setup.sh
@@ -1,7 +1,7 @@
 # Clone the base repo
-yum install git -y && git clone https://github.com/kenaniah/digital-ocean-devops.git
+yum install git -y && git clone https://github.com/kenaniah/virtual-devops.git
 
-cd digital-ocean-devops
+cd virtual-devops
 
 # Ignore file modes and make shell scripts executable
 git config core.filemode false
@@ -10,4 +10,4 @@ chmod 744 *.sh
 cd ..
 
 # Start the party
-digital-ocean-devops/setup.sh
+virtual-devops/setup.sh
