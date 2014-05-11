93bde850c346a9321fdd5b348bcd8f02ea15793b
Kenaniah Cerny <kenaniah@gmail.com>
2014-05-11 10:59:20 -0700

    Added base mongodb module

---
+++ puppet-manifests/production/modules/mongodb/manifests/init.pp
@@ -0,0 +1,14 @@
+class mongodb {
+	
+	file {
+		"/etc/yum.repos.d/mongodb.repo":
+			ensure => file,
+			source => [ "puppet:///modules/mongodb/etc/yum.repos.d/mongodb.repo" ]
+	}
+	
+	package { ["mongo-10gen", "mongo-10gen-server"]:
+		ensure => present,
+		require => File["/etc/yum.repos.d/mongodb.repo"]
+	}
+	
+}
