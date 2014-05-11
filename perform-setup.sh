aceadad388556d86845257e15c4e516135829d09
Kenaniah Cerny <kenaniah@gmail.com>
2014-05-11 07:58:21 -0700

    Now targets `/opt/virtual-devops`

--- perform-setup.sh
+++ perform-setup.sh
@@ -1,7 +1,7 @@
 # Clone the base repo
-yum install git -y && git clone https://github.com/kenaniah/virtual-devops.git
+yum install git -y && git clone https://github.com/kenaniah/virtual-devops.git /opt/virtual-devops
 
-cd virtual-devops
+cd /opt/virtual-devops
 
 # Ignore file modes and make shell scripts executable
 git config core.filemode false
@@ -12,7 +12,5 @@ test -n "$1" && git checkout "$1"
 # Set all shell scripts to executable
 chmod 744 *.sh
 
-cd ..
-
 # Start the party
-virtual-devops/setup.sh
+./setup.sh
