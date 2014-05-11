079461fba6ae600cc05bb69fdc766e9203924ca9
Kenaniah Cerny <kenaniah@gmail.com>
2014-05-11 00:46:14 -0700

    Fixed PHP repo

--- puppet-manifests/production/modules/php/manifests/init.pp
+++ puppet-manifests/production/modules/php/manifests/init.pp
@@ -10,6 +10,20 @@ class php {
 		creates => "/etc/yum.repos.d/remi.repo"
 	}
 	
+	exec { "rpmforge yum repo":
+		command => "rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm",
+		creates => "/etc/yum.repos.d/rpmforge.repo"
+	}
+	
+	ini_setting { "remi base":
+		ensure => present,
+		path => "/etc/yum.repos.d/remi.repo",
+		section => "remi",
+		setting => "enabled",
+		value => 1,
+		require => Exec["remi yum repo"]
+	}
+	
 	ini_setting { "remi php 5.5":
 		ensure => present,
 		path => "/etc/yum.repos.d/remi.repo",
@@ -20,7 +34,7 @@ class php {
 	}
 
 	package { [
-		"gcc", 
+		"gcc",
 		"php",
 		"php-common",
 		"php-cli",
@@ -31,6 +45,7 @@ class php {
 		"php-mbstring",
 		"php-mysqlnd",
 		"php-pdo",
+		"php-pear",
 		"php-pecl-gmagick",
 		# "php-pecl-imagick",
 		"php-pecl-memcached",
