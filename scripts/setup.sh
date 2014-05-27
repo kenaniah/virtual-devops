bc83e3611b39bc616d4e5b4630177f0e4fa4eec3
Kenaniah Cerny <kenaniah@gmail.com>
2014-05-26 20:42:36 -0700

    Checkpoint

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,27 +16,35 @@ rm /etc/localtime -f && ln -s /usr/share/zoneinfo/$TZ /etc/localtime
 
 # Install repositories
 rpm -ihv http://mirror.steadfast.net/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm -ihv http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-
-# Install base packages
-yum install vim wget man htop puppet bind-utils mlocate yum-plugin-versionlock -y
-#yum versionlock puppet facter
 
 # Point to the puppet host (if it doesn't already exist)
 grep -q "puppet" /etc/hosts || echo "$PUPPET_IP puppet" >> /etc/hosts
+
+# Determine if we are a puppet master or client
+if [ `hostname` = "puppet" ]; then
+	. $SCRIPT_PATH/scripts/puppet-master.sh
+else
+	. $SCRIPT_PATH/scripts/puppet-client.sh
+fi
+
+#rpm -ihv http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+
+# Install base packages
+#yum install vim wget man htop puppet bind-utils mlocate yum-plugin-versionlock -y
+#yum versionlock puppet facter
 
 # Perform a full system update
 yum update -y
 
 # Copy over the puppet config files
-rsync -v $SCRIPT_PATH/files/auth.conf /etc/puppet/auth.conf
-rsync -v $SCRIPT_PATH/files/puppet.conf /etc/puppet/puppet.conf
+#rsync -v $SCRIPT_PATH/files/auth.conf /etc/puppet/auth.conf
+#rsync -v $SCRIPT_PATH/files/puppet.conf /etc/puppet/puppet.conf
 
 # Set up the puppet master (if our IPs match)
-. $SCRIPT_PATH/scripts/puppetmaster.sh
+#. $SCRIPT_PATH/scripts/puppetmaster.sh
 
 # Perform the setup
-puppet agent --test --waitforcert 60
+#puppet agent --test --waitforcert 60
 
 # Start the puppet client service
-puppet resource service puppet ensure=running enable=true
+#puppet resource service puppet ensure=running enable=true
