309b8b22b91da02414470de6a87b474bea89cf57
Kenaniah Cerny <kenaniah@gmail.com>
2014-05-28 21:27:58 -0700

    Puppet config now copied to nodes

--- scripts/puppet-client.sh
+++ scripts/puppet-client.sh
@@ -1,5 +1,6 @@
 # Copy over the puppet configs
 rsync -v $SCRIPT_PATH/files/auth.conf /etc/puppet/auth.conf
+rsync -v $SCRIPT_PATH/files/puppet.conf /etc/puppet/puppet.conf
 
 # Perform a puppet run
 puppet agent --test
